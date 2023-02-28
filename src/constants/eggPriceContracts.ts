cf1a1ca8328f3b0f9f50beb6efdbe2a6497d8642
trytryx <100495047+trytryx@users.noreply.github.com>
2023-02-28 21:16:52 +0300

    Update eggPriceContracts.ts

--- src/constants/eggPriceContracts.ts
+++ src/constants/eggPriceContracts.ts
@@ -1,7 +1,7 @@
 const priceContracts: {cakeAddress: string, busdAddress: string, lpAddress:string} = {
-  cakeAddress: '0xAB9D0Fae6eB062F2698C2D429a1BE9185A5D4F6E',
+  cakeAddress: '0x85EAC5Ac2F758618dFa09bDbe0cf174e7d574D5B',
   busdAddress: '0xe9e7cea3dedca5984780bafc599bd69add087d56',
-  lpAddress: '0x788fdceb07AFABB2e0fd920651D292088Fb96752'
+  lpAddress: '0xb5D108578Be3750209d1b3A8f45FFee8C5a75146'
 }
 
 export default priceContracts
